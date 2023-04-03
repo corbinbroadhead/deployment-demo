04a55a91e55f71d292008474760860884a0e2fc8
Corbin Broadhead <poujmail@gmail.com>
2023-04-03 15:18:51 -0600

    new name

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 const btn = document.querySelector('button')
 
-const clickHandler = () => alert('The cat name is: Rascal')
+const clickHandler = () => alert('The cat name is: Pillbug')
 
 btn.addEventListener('click', clickHandler)
